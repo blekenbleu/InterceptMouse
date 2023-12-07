8cf22b7b90b814496e0e1746fa7b53913330f587
Steven Carmichael <blekenbleu@gmail.com>
2023-12-07 10:22:56 -0500

    whitespace; redundant failure message

--- Program.cs
+++ Program.cs
@@ -8,11 +8,13 @@ namespace InterceptMouse
 		static void Main()
 		{
 			if (!InputInterceptor.Initialize()) {
-				Console.WriteLine("Input interceptor not initialized;  probably invalid " + InputInterceptor.DPath);
+				Console.WriteLine("Input interceptor not initialized;"
+								+ "  probably invalid " + InputInterceptor.DPath);
 				return;
 			}
+
 			Intercept Mouse = new();
-			if(Mouse.Initialize(Console.WriteLine)) // Intercept instance will use Console.WriteLine()
+			if(Mouse.Initialize(Console.WriteLine))
 			{
 				Console.WriteLine("Mouse Hook enabled. Press any keyboard key to release.");
 				Console.ReadKey();
@@ -22,7 +24,6 @@ namespace InterceptMouse
 				Console.WriteLine($"..\n..");
 				Console.WriteLine($"Hooks released;  Mouse count = {Mouse.Count}\n");
 			}
-			else Console.WriteLine("Interception failed.");
 
 			Console.WriteLine("Press any key to exit.");
 			Console.ReadKey();
